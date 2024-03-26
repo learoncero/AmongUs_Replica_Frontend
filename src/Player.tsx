ee494e23d5ea39c8b404afc558d5132989dd76ba
Sarah Neville <sarah.neville@students.fhv.at>
2024-03-26 10:24:07 +0100

    imported Player component from branch playerMovement

---
+++ src/Player.tsx
@@ -0,0 +1,22 @@
+import { Player } from './App';
+
+type Props = {
+    player: Player;
+};
+
+export default function PlayerIcon({ player }: Props) {
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                left: player.x,
+                top: player.y,
+                width: '50px',
+                height: '50px',
+                backgroundColor: 'red',
+            }}
+        >
+            {player.username}
+        </div>
+    );
+}
