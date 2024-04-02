65bca0d4b47ae9706137724c5b9b0306be3a2b0b
Lea Roncero <lea.roncero@students.fhv.at>
2024-04-02 12:19:04 +0200

    added Back buttons in gameŚetup and joinGame

--- src/at.fhv.GameSetup/JoinGame.tsx
+++ src/at.fhv.GameSetup/JoinGame.tsx
@@ -17,33 +17,38 @@ export default function JoinGame() {
 
   return (
     <div className="bg-black flex justify-center items-center h-screen">
-      <div className="w-96 flex flex-col space-y-4">
-        <input
-          type="text"
-          placeholder="Enter your name"
-          value={playerName}
-          onChange={handlePlayerNameChange}
-          className="bg-transparent border border-white text-white font-bold py-3 rounded-lg text-xl text-center"
-        />
-        <input
-          type="text"
-          placeholder="Enter game code"
-          value={gameCode}
-          onChange={handleGameCodeChange}
-          maxLength={6}
-          className="bg-transparent border border-white text-white font-bold py-3 rounded-lg text-xl text-center"
-        />
-        <Link
-          to={isJoinDisabled ? "#" : `/lobby/${gameCode}`} // Assuming the game code will be part of the route
-          className={`bg-transparent border border-white text-white font-bold py-3 rounded-lg text-xl text-center ${
-            !isJoinDisabled
-              ? "hover:border-black hover:bg-cyan-500"
-              : "cursor-default"
-          }`}
-          onClick={(e) => isJoinDisabled && e.preventDefault()} // Prevent navigation if the button is disabled
-        >
-          JOIN GAME
+      <div className="max-w-md text-white p-8 rounded-lg border-white border">
+        <Link to="/chooseGameMode" className="text-white text-lg mb-4 block">
+          Back
         </Link>
+        <div className="w-96 flex flex-col space-y-4">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={playerName}
+            onChange={handlePlayerNameChange}
+            className="bg-transparent border border-white text-white font-bold py-3 rounded-lg text-xl text-center"
+          />
+          <input
+            type="text"
+            placeholder="Enter game code"
+            value={gameCode}
+            onChange={handleGameCodeChange}
+            maxLength={6}
+            className="bg-transparent border border-white text-white font-bold py-3 rounded-lg text-xl text-center"
+          />
+          <Link
+            to={isJoinDisabled ? "#" : `/lobby/${gameCode}`} // Assuming the game code will be part of the route
+            className={`bg-transparent border border-white text-white font-bold py-3 rounded-lg text-xl text-center ${
+              !isJoinDisabled
+                ? "hover:border-black hover:bg-cyan-500"
+                : "cursor-default"
+            }`}
+            onClick={(e) => isJoinDisabled && e.preventDefault()} // Prevent navigation if the button is disabled
+          >
+            JOIN GAME
+          </Link>
+        </div>
       </div>
     </div>
   );
